50a6b23454f0cf22dc1eb80c4b7d147230d6eec4
loganzimm4 <101672520+loganzimm4@users.noreply.github.com>
2022-04-13 16:12:56 -0400

    commit tree 85f1c4e2b35d92ba7bc4f805432b178c3d226496

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('%c HI', 'color: firebrick')
+fetch ('https://dog.ceo/api/breeds/image/random/4')
+    .then(res => res.json())
+    .then(data => renderImg(data.message))
+
+    renderImg(dataArr)
+    dataArr.forEach(Url => {
+        const img = document.createElement('img')
+        img.src = Url
+        document.getElementById('dog-image-container').appendChild(img)
+    })
